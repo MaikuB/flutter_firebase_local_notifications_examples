79576770d505cbeeeacdcc55a3ed84e3892acd2f
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2020-03-09 22:21:34 +1100

    add folder for Swift/Kotlin example, bump dependencies for ObjC example (#3)
    * add swift kotlin app
    
    * update swift app with correct capabilities configured
    
    * ignore Googleservice-info.plist for Swift app

---
+++ kotlin_swift/lib/main.dart
@@ -0,0 +1,137 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _token;
+  String _launchMessage;
+  @override
+  void initState() {
+    super.initState();
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    _firebaseMessaging.configure(onMessage: (message) async {
+      print('onMessage: $message');
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id',
+        'your channel name',
+        'your channel description',
+        importance: Importance.Max,
+        priority: Priority.High,
+      );
+      var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await _flutterLocalNotificationsPlugin.show(
+        0,
+        message['aps']['alert']['title'],
+        message['aps']['alert']['body'],
+        platformChannelSpecifics,
+      );
+    }, onLaunch: (message) async {
+      setState(() {
+        _launchMessage = message.toString();
+      });
+      print('onLaunch: $message');
+    }, onResume: (message) async {
+      print('onResume: $message');
+    });
+    final result = _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true)) as Future<bool>;
+    if (result != null) {
+      result.then((requested) {
+        print('permissioned requested $requested');
+        var initializationSettingsIOS = IOSInitializationSettings(
+            onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+        var initializationSettings = InitializationSettings(
+            initializationSettingsAndroid, initializationSettingsIOS);
+        _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+            onSelectNotification: onSelectNotification);
+      });
+    }
+    _firebaseMessaging.getToken().then((token) {
+      print('token: $token');
+      if (mounted) {
+        setState(() {
+          _token = token;
+        });
+      } else {
+        _token = token;
+      }
+    });
+  }
+
+  Future<void> onSelectNotification(String payload) async {
+    debugPrint('onSelectNotification');
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+  }
+
+  Future<void> onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase sample'),
+      ),
+      body: Column(
+        children: [
+          Text('FCM token: ${_token ?? ''}'),
+          Text('Launch message: ${_launchMessage ?? ''}'),
+          RaisedButton(
+            child: Text('show local notification'),
+            onPressed: () async {
+              var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+                'your channel id',
+                'your channel name',
+                'your channel description',
+                importance: Importance.Max,
+                priority: Priority.High,
+              );
+              var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+              var platformChannelSpecifics = NotificationDetails(
+                  androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+              await _flutterLocalNotificationsPlugin.show(
+                  1, 'plain title', 'plain body', platformChannelSpecifics,
+                  payload: 'item x');
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  FutureOr onValue(bool value) {}
+}
