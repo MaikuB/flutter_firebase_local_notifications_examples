7218d44140c5cac214a8fd09a6c7f887923c66fc
Michael Bui <25263378+MaikuB@users.noreply.github.com>
2021-07-03 15:18:22 +1000

    Bump dependencies for Kotlin/Swift example and migrate to null safety (#5)
    * bump flutter_local_notifications dependency
    
    * bump kotlin swift example
    
    * have Podfile ignored
    
    * use shared preferences to save background message and allow Podfile to be committed so it can set the global platform version of 10.0
    
    * migrate to null safety
    
    * updated readme for kotlin swift example
    
    * clear shared preferences after reading background message id from it

--- kotlin_swift/lib/main.dart
+++ kotlin_swift/lib/main.dart
@@ -1,15 +1,37 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+const String backgroundMessageIdKey = 'backgroundMessageId';
+
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
+RemoteMessage? initialRemoteMessage;
+NotificationAppLaunchDetails? launchNotificationDetails;
 
-void main() => runApp(MyApp());
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  final SharedPreferences sharedPreferences =
+      await SharedPreferences.getInstance();
+  sharedPreferences.setString(backgroundMessageIdKey, message.messageId!);
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  initialRemoteMessage = await _firebaseMessaging.getInitialMessage();
+
+  launchNotificationDetails =
+      await _flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -26,69 +48,93 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _token;
-  String _launchMessage;
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  String? _token;
+  String? selectedLocalNotification = '';
+  String? selectedRemoteNotification = '';
+  String? backgroundMessageId = '';
+
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance!.addObserver(this);
     initializePlugins();
   }
 
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      final SharedPreferences sharedPreferences =
+          await SharedPreferences.getInstance();
+      if (sharedPreferences.containsKey(backgroundMessageIdKey)) {
+        backgroundMessageId =
+            sharedPreferences.getString(backgroundMessageIdKey);
+        if (mounted) {
+          setState(() {});
+        }
+        sharedPreferences.clear();
+      }
+    }
+  }
+
   Future<void> initializePlugins() async {
-    _firebaseMessaging.configure(onMessage: (message) async {
+    await Firebase.initializeApp();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    await _firebaseMessaging.requestPermission(
+        sound: true, badge: true, alert: true);
+    await initializeFlutterLocalNotificationsPlugin();
+    await _firebaseMessaging.setForegroundNotificationPresentationOptions(
+        alert: true, badge: true, sound: true);
+
+    FirebaseMessaging.onMessage.listen((message) async {
       print('onMessage: $message');
+      setState(() {
+        selectedRemoteNotification = message.notification!.body;
+      });
+      if (Platform.isIOS) {
+        return;
+      }
       var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'your channel id',
         'your channel name',
         'your channel description',
-        importance: Importance.Max,
-        priority: Priority.High,
+        importance: Importance.max,
+        priority: Priority.high,
       );
       var iOSPlatformChannelSpecifics = IOSNotificationDetails();
       var platformChannelSpecifics = NotificationDetails(
-          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-      var title = '';
-      if (Platform.isIOS) {
-        title = message['aps']['alert']['title'];
-      } else if (Platform.isAndroid) {
-        title = message['notification']['title'];
-      }
-
-      var body = '';
-      if (Platform.isIOS) {
-        body = message['aps']['alert']['body'];
-      } else if (Platform.isAndroid) {
-        body = message['notification']['body'];
-      }
+          android: androidPlatformChannelSpecifics,
+          iOS: iOSPlatformChannelSpecifics);
       await _flutterLocalNotificationsPlugin.show(
         0,
-        title,
-        body,
+        message.notification!.title,
+        message.notification!.body,
         platformChannelSpecifics,
       );
-    }, onLaunch: (message) async {
-      setState(() {
-        _launchMessage = message.toString();
-      });
-      print('onLaunch: $message');
-    }, onResume: (message) async {
-      print('onResume: $message');
     });
-    final result = await _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(sound: true, badge: true, alert: true));
-    if (result != null) {
-      print('permissioned requested $result');
-      await initializeFlutterLocalNotificationsPlugin();
-    } else if (Platform.isAndroid) {
-      await initializeFlutterLocalNotificationsPlugin();
-    }
+
+    _firebaseMessaging.onTokenRefresh.listen((token) {
+      _saveToken(token);
+    });
+
     final token = await _firebaseMessaging.getToken();
+    _saveToken(token);
+  }
+
+  void _saveToken(String? token) {
     print('token: $token');
     if (mounted) {
       setState(() {
@@ -105,12 +151,15 @@ class _MyHomePageState extends State<MyHomePage> {
     var initializationSettingsIOS = IOSInitializationSettings(
         onDidReceiveLocalNotification: onDidReceiveLocalNotification);
     var initializationSettings = InitializationSettings(
-        initializationSettingsAndroid, initializationSettingsIOS);
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
     await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
         onSelectNotification: onSelectNotification);
   }
 
-  Future<void> onSelectNotification(String payload) async {
+  Future<void> onSelectNotification(String? payload) async {
+    setState(() {
+      selectedLocalNotification = payload;
+    });
     debugPrint('onSelectNotification');
     if (payload != null) {
       debugPrint('notification payload: ' + payload);
@@ -118,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> onDidReceiveLocalNotification(
-      int id, String title, String body, String payload) async {}
+      int id, String? title, String? body, String? payload) async {}
 
   @override
   Widget build(BuildContext context) {
@@ -128,21 +177,28 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          Text('FCM token: ${_token ?? ''}'),
-          Text('Launch message: ${_launchMessage ?? ''}'),
-          RaisedButton(
+          SelectableText('FCM token: ${_token ?? ''}'),
+          Text(
+              'Initial remote message: ${(initialRemoteMessage?.notification?.body ?? '')}'),
+          Text(
+              'Launch notification payload: ${(launchNotificationDetails?.payload ?? '')}'),
+          Text('Background message id: $backgroundMessageId'),
+          Text('Selected remote message: $selectedRemoteNotification'),
+          Text('Selected local message: $selectedLocalNotification'),
+          ElevatedButton(
             child: Text('show local notification'),
             onPressed: () async {
               var androidPlatformChannelSpecifics = AndroidNotificationDetails(
                 'your channel id',
                 'your channel name',
                 'your channel description',
-                importance: Importance.Max,
-                priority: Priority.High,
+                importance: Importance.max,
+                priority: Priority.high,
               );
               var iOSPlatformChannelSpecifics = IOSNotificationDetails();
               var platformChannelSpecifics = NotificationDetails(
-                  androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+                  android: androidPlatformChannelSpecifics,
+                  iOS: iOSPlatformChannelSpecifics);
               await _flutterLocalNotificationsPlugin.show(
                   1, 'plain title', 'plain body', platformChannelSpecifics,
                   payload: 'item x');
@@ -152,6 +208,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-  FutureOr onValue(bool value) {}
 }
